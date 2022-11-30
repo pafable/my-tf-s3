e6a79addb4da55d14e9d56c0c45c0237feaa502b
phil <philafable@gmail.com>
2022-11-29 22:33:22 -0500

    removed provider

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_version = ">= 1.2"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
 locals {
   tags        = tomap({ "Name" = var.bucket_name, "owner" = var.owner, "environment" = terraform.workspace })
   bucket_name = "${var.bucket_name}-${var.region}-${terraform.workspace}"
